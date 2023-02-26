934d0054050621c24ba440c8f1dabd83f25430bd
roberto <roberbaldovino167@gmail.com>
2023-02-25 21:26:19 -0500

    commit two validate routes

--- src/Routes/language.routes.js
+++ src/Routes/language.routes.js
@@ -4,5 +4,5 @@ const router = Router();
 router.get('/', function(req,res) {
     res.send("hello world");
 });
-    
+// hola mundo
 export default router;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ const main =()=>{
    app.listen(app.get('port'));
    console.log("Server on port ", app.get('port'));
 };
-  
+//run main function
 main();
